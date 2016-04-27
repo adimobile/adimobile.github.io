60af30216506c3b12a8e1e81de839b0f2ff6ece8
Pedro Fraca Tarancon <pedro.fraca@gmail.com>
2016-04-27 16:47:35 +0200

    generate python file for gen html main file and added some sample version folders

---
+++ gen.py
@@ -0,0 +1,31 @@
+
+from dominate import document
+from dominate.tags import *
+import os
+print "root prints out directories only from what you specified"
+print "dirs prints out sub-directories from root"
+print "files prints out all files from root and directories"
+print "*" * 20
+
+
+root={}
+for item in os.listdir("."):
+    if not os.path.isfile(os.path.join(".", item)):
+        if (item != ".git"):
+            print "Folder: ",item
+            subfolders=[]
+            for item2 in os.listdir(os.path.join(".", item)):
+                if not os.path.isfile(os.path.join(".", item2)):
+                    print "subFolder: ",item2
+                    subfolders.append(item2)
+            root[item]=subfolders
+
+with document(title='adimobile javadoc') as doc:
+    for folder in root:
+        h1(folder)
+        for sub in root[folder]:
+            div(a(sub, href='%s/%s/index.html' % (folder,sub)))
+
+with open('index.html', 'w') as f:
+    f.write(doc.render())
+print root
